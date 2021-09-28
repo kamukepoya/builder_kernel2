19926852d9e6772f56067b17567e8422ba419be9
AL1 <76146196+SeeAli@users.noreply.github.com>
2021-09-28 08:05:01 +0700

    Update kernel_build.sh

--- kernel_build.sh
+++ kernel_build.sh
@@ -18,7 +18,7 @@
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth=1 $KERNEL_SOURCE $KERNEL_BRANCH $DEVICE_CODENAME
-git clone --depth=1 https://github.com/cbendot/elastics-toolchain clang
+git clone --depth=1 https://gitlab.com/zephyc/clang clang
 
 # Main Declaration
 KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
