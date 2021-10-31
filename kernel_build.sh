d50c9b93ee778b3c44fd685be63bef6040997523
ali <wowsanja2@gmail.com>
2021-10-31 13:23:31 +0000

    RucikaGitu

--- kernel_build.sh
+++ kernel_build.sh
@@ -71,7 +71,7 @@ make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
     CC=${CLANG_ROOTDIR}/bin/clang \
     NM=${CLANG_ROOTDIR}/bin/llvm-nm \
-    OLD=${CLANG_ROOTDIR}/bin/ld.lld \
+    LD=${CLANG_ROOTDIR}/bin/ld.lld \
     CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
 
@@ -107,7 +107,7 @@ function finerr() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Niki-Kernel-OSS.zip *
+    zip -r9 Rocket-Kernel-OSS.zip *
     cd ..
 }
 check
