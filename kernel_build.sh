46957aca960ccd4417ef90c5240ec7dc7db8c56e
TianWalkzzMiku0 <77526226+kentanglu@users.noreply.github.com>
2021-11-26 02:22:34 +0700

    Update kernel_build.sh

--- kernel_build.sh
+++ kernel_build.sh
@@ -11,7 +11,6 @@
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth=1 https://github.com/kentanglu/Rocket_Kernel_MT6768 -b eleven
-then
 git clone --depth=1 https://github.com/GengKapak/GengKapak-clang -b 12 clang
 else
 git clone --depth=1 https://github.com/Asyanx/AnyKernel3.1 -b master AnyKernel
