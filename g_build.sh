552da3f8e25ad5e558795a6a6897c7a0c3d30c8d
TianWalkzzMiku0 <77526226+kentanglu@users.noreply.github.com>
2021-11-28 14:42:56 +0700

    Update g_build.sh

--- g_build.sh
+++ g_build.sh
@@ -52,8 +52,6 @@ tg_post_msg() {
 # Compile
 compile(){
 cd ${KERNEL_ROOTDIR}
-echo "CONFIG_CC_STACKPROTECTOR_STRONG=y" >> arch/arm64/configs/"merlin_defconfig"
-echo "CONFIG_COMPAT_VDSO=y" >> arch/arm64/configs/"merlin_defconfig"
 make -j$(nproc) O=out ARCH=arm64 merlin_defconfig
 make -j$(nproc) ARCH=arm64 O=out \
     CC=clang \
