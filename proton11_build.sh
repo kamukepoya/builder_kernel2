e6578687740fd45459db5ef82a6ebbca19f2249a
TianWalkzzMiku0 <77526226+kentanglu@users.noreply.github.com>
2021-11-28 18:51:51 +0700

    Update proton11_build.sh

--- proton11_build.sh
+++ proton11_build.sh
@@ -42,6 +42,8 @@ compile(){
 cd ${KERNEL_ROOTDIR}
 echo "CONFIG_CC_STACKPROTECTOR_STRONG=y" >> arch/arm64/configs/"merlin_defconfig"
 echo "CONFIG_COMPAT_VDSO=y" >> arch/arm64/configs/"merlin_defconfig"
+echo "CONFIG_ARM_ARCH_TIMER_VCT_ACCESS=y" >> arch/arm64/configs/"merlin_defconfig"
+echo "CONFIG_KUSER_HELPERS=y" >> arch/arm64/configs/"merlin_defconfig"
 make -j$(nproc) O=out ARCH=arm64 merlin_defconfig
 make -j$(nproc) ARCH=arm64 O=out \
     CC=${CLANG_ROOTDIR}/bin/clang \
