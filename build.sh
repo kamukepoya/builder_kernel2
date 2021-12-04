18a3413759933435c5ffcac9fa2dc66269225dd2
Asyanx <wowsanja2@gmail.com>
2021-12-04 22:43:20 +0700

    apalah

--- build.sh
+++ build.sh
@@ -2,15 +2,19 @@
 
 export KERNELNAME=Sea-Kernel
 
-send_msg "⏳ Start building ${KERNELNAME} | DEVICE: Merlinx / Merlin"
+tg_post_msg "⏳ Start building ${KERNELNAME} | DEVICE: Merlinx / Merlin"
 
 START=$(date +"%s")
 
-source compiler/clang/Gclang.sh , GPkclang.sh
-
+#com
+com() {
+        - bash compiler/clang/Gclang.sh
+        - bash compiler/clang/GPKclang.sh
+}
+com
 END=$(date +"%s")
 
 DIFF=$(( END - START ))
 
-send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s"
+tg_post_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s"
 
