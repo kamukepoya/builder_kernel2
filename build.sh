4f72db2961d3f9851834cbf1c4beb40bfd5aeba2
TianWalkzzMiku0 <77526226+kentanglu@users.noreply.github.com>
2021-12-04 22:48:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-export KERNELNAME=Sea-Kernel
-
-tg_post_msg "⏳ Start building ${KERNELNAME} | DEVICE: Merlinx / Merlin"
-
 START=$(date +"%s")
 
 source() {
         - source compiler/clang/Gclang.sh
         - source compiler/clang/GPKclang.sh
 }
+source
 
 END=$(date +"%s")
 
 DIFF=$(( END - START ))
-
-tg_post_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s"
 
